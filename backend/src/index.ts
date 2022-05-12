07f24f240e333289ad6ec9305fb7403f8f207ff7
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-05-12 14:59:54 +0200

    added ts to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,11 @@
-const { app, BrowserWindow, ipcMain, Menu } = require('electron')
+import path from "path"
+import fs from 'fs'
 
-const isDev = process.env.NODE_ENV === 'local'
+import contextMenu from 'electron-context-menu'
+import {app, BrowserWindow, ipcMain, Menu, MenuItemConstructorOptions} from 'electron'
+
 const isMac = process.platform === 'darwin'
+const isDev = process.env.NODE_ENV === 'local'
 
 const template = [
     // { role: 'appMenu' }
@@ -104,7 +108,63 @@ const template = [
     }
 ]
 
-const menu = Menu.buildFromTemplate(template)
+const menu = Menu.buildFromTemplate(template as MenuItemConstructorOptions[])
 
+const isDebugProduction = true // Set to True to debug
+if (isDev) require('electron-reloader')(module)
+let mainWindow
 
-module.exports = menu
+contextMenu({showInspectElement: isDev});
+
+Menu.setApplicationMenu(menu)
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: isDev || isDebugProduction ? 1000 : 800,
+        height: 600,
+        x: 0,
+        y: 0,
+        title: isDev ? "DEV MODE" : "Todo Today",
+        webPreferences: {
+            nodeIntegration: true,
+            // enableRemoteModule: true,
+            contextIsolation: false,
+            devTools: isDev || isDebugProduction,
+            spellcheck: true
+        }
+    })
+
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+        mainWindow.loadURL('http://localhost:3003')
+    } else {
+        if (isDebugProduction) mainWindow.webContents.openDevTools();
+        mainWindow.loadFile(path.resolve(__dirname, 'react-dist', 'index.html'))
+
+    }
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+ipcMain.handle('backup', async (event, arg) => {
+    try {
+        const dirname = path.resolve(app.getPath('appData'), app.name, 'backups')
+        if (!fs.existsSync(dirname)) {
+            fs.mkdirSync(dirname);
+        }
+        fs.writeFileSync(path.resolve(dirname, arg.filename), arg.data, 'utf8');
+        return { isSuccess: true }
+    } catch (e) {
+        return { isSuccess: false, message: JSON.stringify(e) }
+    }
+})
