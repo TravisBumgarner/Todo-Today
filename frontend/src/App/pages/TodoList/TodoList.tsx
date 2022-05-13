088ee4ef2d0f6ab431f91860f2e4ba2ad69d3ea8
Travis Bumgarner <travis.bumgarner@gmail.com>
2022-05-13 12:17:29 +0200

    completed feature

--- frontend/src/App/pages/TodoList/TodoList.tsx
+++ frontend/src/App/pages/TodoList/TodoList.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import moment from 'moment'
 import { useLiveQuery } from 'dexie-react-hooks'
+import { v4 as uuid4 } from 'uuid'
 
-import { Button, ButtonWrapper, Heading } from 'sharedComponents'
+import { Button, ButtonWrapper, ConfirmationModal, Heading } from 'sharedComponents'
 import { formatDateDisplayString, formatDateKeyLookup, formatDurationDisplayString, sumArray } from 'utilities'
 import database from 'database'
 import { TDateISODate } from 'sharedTypes'
@@ -12,6 +13,7 @@ import { TodoListTable, ManageTodoListItemsModal } from './components'
 const TodoList = () => {
     const [selectedDate, setSelectedDate] = React.useState<TDateISODate>(formatDateKeyLookup(moment()))
     const [showManagementModal, setShowManagementModal] = React.useState<boolean>(false)
+    const [showNothingToCopyModal, setShowNothingToCopyModal] = React.useState<boolean>(false)
     const { state: { dateFormat } } = React.useContext(context)
 
     const todoListItems = useLiveQuery(
@@ -23,14 +25,41 @@ const TodoList = () => {
         [selectedDate]
     )
 
-    const getPreviousDay = () => {
+    const setPreviousDate = () => {
         setSelectedDate(formatDateKeyLookup(moment(selectedDate).subtract(1, 'day')))
     }
+
+    const getNextDate = () => {
+        setSelectedDate(formatDateKeyLookup(moment(selectedDate).add(1, 'day')))
+    }
+
     const getToday = () => {
         setSelectedDate(formatDateKeyLookup(moment()))
     }
-    const getNextDay = () => {
-        setSelectedDate(formatDateKeyLookup(moment(selectedDate).add(1, 'day')))
+
+    const getPreviousDatesTasks = async () => {
+        const previousDay = await database
+            .todoListItems
+            .where('todoListDate')
+            .equals(formatDateKeyLookup(moment(selectedDate).subtract(1, 'days')))
+            .toArray()
+
+        if(previousDay.length === 0 ){
+            setShowNothingToCopyModal(true)
+        } else (
+            previousDay.map(async ({ projectId, taskId }) => {
+                await database.todoListItems.add({
+                    projectId,
+                    taskId,
+                    duration: 0,
+                    id: uuid4(),
+                    todoListDate: selectedDate
+                })
+            })
+        )
+
+       
+
     }
 
     const hoursWorkedSelectedDate = todoListItems
@@ -42,16 +71,24 @@ const TodoList = () => {
             <Heading.H2>{formatDateDisplayString(dateFormat, selectedDate)} {hoursWorkedSelectedDate}</Heading.H2>
             <ButtonWrapper
                 left={[
-                    <Button key="today" onClick={getToday} variation="PRIMARY_BUTTON">Today</Button>,
+                    <Button key="today" disabled={todoListItems && todoListItems.length > 0} onClick={getPreviousDatesTasks} variation="PRIMARY_BUTTON">Copy Yesterday</Button>
                 ]}
                 right={[
-                    <Button key="previous" onClick={getPreviousDay} variation="PRIMARY_BUTTON">Previous</Button>,
-                    <Button key="next" onClick={getNextDay} variation="PRIMARY_BUTTON">Next</Button>,
+                    <Button key="today" onClick={getToday} variation="PRIMARY_BUTTON">Today</Button>,
+                    <Button key="previous" onClick={setPreviousDate} variation="PRIMARY_BUTTON">&lt;</Button>,
+                    <Button key="next" onClick={getNextDate} variation="PRIMARY_BUTTON">&gt;</Button>,
                 ]}
             />
             <TodoListTable todoListItems={todoListItems} selectedDate={selectedDate} />
             <Button key="manage" fullWidth onClick={() => setShowManagementModal(true)} variation="PRIMARY_BUTTON">Add Tasks</Button>
             <ManageTodoListItemsModal selectedDate={selectedDate} showModal={showManagementModal} setShowModal={setShowManagementModal} />
+            <ConfirmationModal
+                body="It looks like there's nothing to copy from yesterday."
+                title="Heads Up!"
+                confirmationCallback={() => setShowNothingToCopyModal(false)}
+                showModal={showNothingToCopyModal}
+                setShowModal={setShowNothingToCopyModal}
+            />
         </>
     )
 }
