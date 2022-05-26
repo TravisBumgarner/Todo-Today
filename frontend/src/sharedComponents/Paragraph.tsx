07ef018e41ce9ad95c480f535eaf68dbbec73c96
Travis Bumgarner <travisbumgarner@traviss-macbook-pro.home>
2022-05-26 12:15:51 +0200

    fixed some issues with fresh install

--- frontend/src/sharedComponents/Paragraph.tsx
+++ frontend/src/sharedComponents/Paragraph.tsx
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 
 const Paragraph = styled.p`
     color: ${({ theme }) => theme.INTERACTION};
-    line-height: 1.5
+    line-height: 1.5;
+    font-family: 'Comfortaa', cursive;
 `
 
 export default Paragraph
