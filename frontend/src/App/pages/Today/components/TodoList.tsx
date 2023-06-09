4b473c292f80b0eaf18d0cd8421885b32b536962
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-06-09 16:59:00 -0600

    Fixed copy previous

--- frontend/src/App/pages/Today/components/TodoList.tsx
+++ frontend/src/App/pages/Today/components/TodoList.tsx
@@ -1,145 +1,144 @@
-import React from 'react'
-import moment from 'moment'
-import { useLiveQuery } from 'dexie-react-hooks'
-import { v4 as uuid4 } from 'uuid'
+import React from 'react';
+import moment from 'moment';
+import { useLiveQuery } from 'dexie-react-hooks';
+import { v4 as uuid4 } from 'uuid';
 
-import { BigBoxOfNothing, Button, ButtonWrapper, ConfirmationModal, Heading } from 'sharedComponents'
-import { formatDateKeyLookup } from 'utilities'
-import database from 'database'
-import { ETaskStatus, TDateISODate } from 'sharedTypes'
-import { AddTaskModal, AddProjectModal } from 'sharedModals'
-import { TodoListTable, ManageTodoListItemsModal } from '.'
+import { BigBoxOfNothing, Button, ButtonWrapper, ConfirmationModal, Heading } from 'sharedComponents';
+import { formatDateKeyLookup } from 'utilities';
+import database from 'database';
+import { ETaskStatus, TDateISODate } from 'sharedTypes';
+import { AddTaskModal, AddProjectModal } from 'sharedModals';
+import { TodoListTable, ManageTodoListItemsModal } from '.';
 
-type TodoListProps = {
-    selectedDate: TDateISODate
-}
+  type TodoListProps = {
+  selectedDate: TDateISODate;
+};
 
 const TodoList = ({ selectedDate }: TodoListProps) => {
-    const [showManagementModal, setShowManagementModal] = React.useState<boolean>(false)
-    const [showNothingToCopyModal, setShowNothingToCopyModal] = React.useState<boolean>(false)
-    const [showAddNewTaskModal, setShowAddNewTaskModal] = React.useState<boolean>(false)
-    const [showAddNewProjectModal, setShowAddNewProjectModal] = React.useState<boolean>(false)
-    const [isReadOnly, toggleIsReadOnly] = React.useState<boolean>(false)
+  const [showManagementModal, setShowManagementModal] = React.useState<boolean>(false);
+  const [showNothingToCopyModal, setShowNothingToCopyModal] = React.useState<boolean>(false);
+  const [showAddNewTaskModal, setShowAddNewTaskModal] = React.useState<boolean>(false);
+  const [showAddNewProjectModal, setShowAddNewProjectModal] = React.useState<boolean>(false);
+  const [isReadOnly, toggleIsReadOnly] = React.useState<boolean>(false);
 
-    const projects = useLiveQuery(() => database.projects.toArray())
-    const tasks = useLiveQuery(() => database.tasks.toArray())
+  const projects = useLiveQuery(() => database.projects.toArray());
+  const tasks = useLiveQuery(() => database.tasks.toArray());
 
-    const todoListItems = useLiveQuery(
-        () => database
-            .todoListItems
-            .where('todoListDate')
-            .equals(selectedDate)
-            .toArray(),
-        [selectedDate]
-    )
+  const todoListItems = useLiveQuery(
+    () => database.todoListItems.where('todoListDate').equals(selectedDate).toArray(),
+    [selectedDate],
+  );
 
-    const getPreviousDatesTasks = async () => {
-        const lastDate = await database.todoListItems.orderBy('todoListDate').last()
-        if (lastDate) {
-            const previousDay = await database
-                .todoListItems
-                .where({
-                    todoListDate: lastDate.todoListDate
-                })
-                .toArray()
+  const getPreviousDatesTasks = async () => {
+    const lastDate = (
+      await database.todoListItems.where('todoListDate').below(selectedDate).sortBy('todoListDate')
+    ).reverse()[0];
 
-            if (previousDay.length === 0) {
-                setShowNothingToCopyModal(true)
-            } else {
-                (
-                    previousDay.map(async ({ projectId, taskId, details }) => {
-                        const task = await database
-                            .tasks
-                            .where('id')
-                            .equals(taskId)
-                            .first()
+    if (lastDate) {
+      const previousDay = await database.todoListItems
+        .where({
+          todoListDate: lastDate.todoListDate,
+        })
+        .toArray();
 
-                        if (
-                            task?.status === ETaskStatus.NEW ||
-                            task?.status === ETaskStatus.IN_PROGRESS ||
-                            task?.status === ETaskStatus.BLOCKED
-                        ) {
-                            await database.todoListItems.add({
-                                projectId,
-                                taskId,
-                                id: uuid4(),
-                                todoListDate: selectedDate,
-                                details
-                            })
-                        }
-                    })
-                )
-            }
-        } else {
-            setShowNothingToCopyModal(true)
-        }
+      if (previousDay.length === 0) {
+        setShowNothingToCopyModal(true);
+      } else {
+        previousDay.map(async ({ projectId, taskId, details }) => {
+          const task = await database.tasks.where('id').equals(taskId).first();
+
+          if (
+            task?.status === ETaskStatus.NEW ||
+            task?.status === ETaskStatus.IN_PROGRESS ||
+            task?.status === ETaskStatus.BLOCKED
+          ) {
+            await database.todoListItems.add({
+              projectId,
+              taskId,
+              id: uuid4(),
+              todoListDate: selectedDate,
+              details,
+            });
+          }
+        });
+      }
+    } else {
+      setShowNothingToCopyModal(true);
     }
+  };
 
-    return (
-        <>
-            <Heading.H3>Todo List</Heading.H3>
-            <ButtonWrapper
-                left={[
-                    <Button
-                        key="today"
-                        disabled={todoListItems && todoListItems.length > 0}
-                        onClick={getPreviousDatesTasks}
-                        variation="INTERACTION"
-                    >
-                        Copy Previous
-                    </Button>,
-                    <Button
-                        key="manage"
-                        disabled={tasks && tasks.length === 0}
-                        title={tasks && tasks.length === 0 ? 'You need to create a task first!' : 'Manage'}
-                        onClick={() => setShowManagementModal(true)}
-                        variation="INTERACTION"
-                    >
-                        Select Tasks
-                    </Button>,
+  return (
+    <>
+      <Heading.H3>Todo List</Heading.H3>
+      <ButtonWrapper
+        left={[
+          <Button
+            key="today"
+            disabled={todoListItems && todoListItems.length > 0}
+            onClick={getPreviousDatesTasks}
+            variation="INTERACTION"
+          >
+            Copy Previous
+          </Button>,
+          <Button
+            key="manage"
+            disabled={tasks && tasks.length === 0}
+            title={tasks && tasks.length === 0 ? 'You need to create a task first!' : 'Manage'}
+            onClick={() => setShowManagementModal(true)}
+            variation="INTERACTION"
+          >
+            Select Tasks
+          </Button>,
+        ]}
+        right={[
+          <Button
+            key="add-project"
+            onClick={() => setShowAddNewProjectModal(true)}
+            variation="INTERACTION"
+            title="Add new Project"
+          >
+            Add New Project
+          </Button>,
+          <Button
+            key="add-task"
+            disabled={projects && projects.length === 0}
+            onClick={() => setShowAddNewTaskModal(true)}
+            variation="INTERACTION"
+            title={projects && projects.length === 0 ? 'You need to create a project first!' : 'Add New Task'}
+          >
+            Add New Task
+          </Button>,
+        ]}
+      />
+      {tasks && tasks.length !== 0 ? (
+        <TodoListTable isReadOnly={isReadOnly} todoListItems={todoListItems} />
+      ) : (
+        <BigBoxOfNothing message="Go create some projects and tasks and come back!" />
+      )}
+      <ManageTodoListItemsModal
+        selectedDate={selectedDate}
+        showModal={showManagementModal}
+        setShowModal={setShowManagementModal}
+      />
+      <ConfirmationModal
+        body="It looks like there's nothing to copy from yesterday."
+        title="Heads Up!"
+        confirmationCallback={() => setShowNothingToCopyModal(false)}
+        showModal={showNothingToCopyModal}
+        setShowModal={setShowNothingToCopyModal}
+      />
+      {showAddNewTaskModal ? (
+        <AddTaskModal
+          selectedDate={selectedDate}
+          showModal={showAddNewTaskModal}
+          setShowModal={setShowAddNewTaskModal}
+        />
+      ) : null}
+      {showAddNewProjectModal ? (
+        <AddProjectModal showModal={showAddNewProjectModal} setShowModal={setShowAddNewProjectModal} />
+      ) : null}
+    </>
+  );
+};
 
-                ]}
-                right={[
-                    <Button
-                        key="add-project"
-                        onClick={() => setShowAddNewProjectModal(true)}
-                        variation="INTERACTION"
-                        title="Add new Project"
-                    >
-                        Add New Project
-                    </Button>,
-                    <Button
-                        key="add-task"
-                        disabled={projects && projects.length === 0}
-                        onClick={() => setShowAddNewTaskModal(true)}
-                        variation="INTERACTION"
-                        title={projects && projects.length === 0 ? 'You need to create a project first!' : 'Add New Task'}
-                    >
-                        Add New Task
-                    </Button>
-                ]}
-            />
-            {tasks && tasks.length !== 0
-                ? <TodoListTable isReadOnly={isReadOnly} todoListItems={todoListItems} />
-                : <BigBoxOfNothing message="Go create some projects and tasks and come back!" />}
-            <ManageTodoListItemsModal selectedDate={selectedDate} showModal={showManagementModal} setShowModal={setShowManagementModal} />
-            <ConfirmationModal
-                body="It looks like there's nothing to copy from yesterday."
-                title="Heads Up!"
-                confirmationCallback={() => setShowNothingToCopyModal(false)}
-                showModal={showNothingToCopyModal}
-                setShowModal={setShowNothingToCopyModal}
-            />
-            {showAddNewTaskModal
-                ? <AddTaskModal selectedDate={selectedDate} showModal={showAddNewTaskModal} setShowModal={setShowAddNewTaskModal} />
-                : null
-            }
-            {showAddNewProjectModal
-                ? <AddProjectModal showModal={showAddNewProjectModal} setShowModal={setShowAddNewProjectModal} />
-                : null
-            }
-        </>
-    )
-}
-
-export default TodoList
+export default TodoList;
