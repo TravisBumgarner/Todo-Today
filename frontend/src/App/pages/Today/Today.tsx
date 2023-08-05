e4befe9ca3bc0ab4a19aba1440612d98098f3914
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-08-05 12:27:25 -0600

    refactored pageheader

--- frontend/src/App/pages/Today/Today.tsx
+++ frontend/src/App/pages/Today/Today.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import moment from 'moment'
-import { Button, Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 
-import { PageHeader } from 'sharedComponents'
 import { formatDateDisplayString, formatDateKeyLookup } from 'utilities'
 import { type TDateISODate } from 'sharedTypes'
 import { TodoList, Successes } from './components'
+import { pageHeaderCSS } from 'theme'
 
 const Today = () => {
     const [selectedDate, setSelectedDate] = React.useState<TDateISODate>(formatDateKeyLookup(moment()))
@@ -24,12 +24,12 @@ const Today = () => {
 
     return (
         <div>
-            <PageHeader>
+            <Box sx={pageHeaderCSS}>
                 <Typography variant="h2">{formatDateDisplayString(selectedDate)}</Typography>
                 <Button key="today" onClick={getToday} >Today</Button>
                 <Button key="previous" onClick={setPreviousDate} >&lt;</Button>
                 <Button key="next" onClick={getNextDate} >&gt;</Button>
-            </PageHeader>
+            </Box>
             <TodoList selectedDate={selectedDate} />
             <Successes selectedDate={selectedDate} />
         </div>
