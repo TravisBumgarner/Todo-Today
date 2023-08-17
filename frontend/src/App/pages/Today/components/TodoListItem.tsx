40f1a271d89c1d149c8beda233f337349d46a587
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-08-17 17:54:48 -0600

    ayyyyy

--- frontend/src/App/pages/Today/components/TodoListItem.tsx
+++ frontend/src/App/pages/Today/components/TodoListItem.tsx
@@ -1,6 +1,6 @@
-import { type ChangeEvent, useState, type MouseEvent } from 'react'
+import { type ChangeEvent, useState, type MouseEvent, useCallback } from 'react'
 import database from 'database'
-import { Box, Container, TextField, Tooltip, Typography, css } from '@mui/material'
+import { Box, Card, FormControlLabel, Switch, TextField, Tooltip, Typography, css } from '@mui/material'
 import PendingIcon from '@mui/icons-material/Pending'
 import ToggleButton from '@mui/material/ToggleButton'
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
@@ -24,8 +24,11 @@ interface TodoListItemProps {
 const TodoListItem = ({ id, taskId, taskStatus: defaultTaskStatus, details: defaultDetails, taskTitle, projectTitle }: TodoListItemProps) => {
   const [taskStatus, setTaskStatus] = useState(defaultTaskStatus)
   const [details, setDetails] = useState(defaultDetails)
+  const [showDetails, setShowDetails] = useState(defaultDetails.length > 0)
 
-  const handleStatusChange = async (
+  const toggleShowDetails = useCallback(() => { setShowDetails(prev => !prev) }, [])
+
+  const handleStatusChange = useCallback(async (
     event: MouseEvent<HTMLElement>,
     status: ETaskStatus
   ) => {
@@ -33,9 +36,9 @@ const TodoListItem = ({ id, taskId, taskStatus: defaultTaskStatus, details: defa
     // of the value locally, and also sync it to Dexie.
     void database.tasks.where('id').equals(taskId).modify({ status })
     setTaskStatus(status)
-  }
+  }, [taskId])
 
-  const handleDetailsChange = async (
+  const handleDetailsChange = useCallback(async (
     event: ChangeEvent<HTMLInputElement>
   ) => {
     // For whatever reason, Dexie does not seem to want to liveSync updates. Therefore, we'll keep track
@@ -43,62 +46,72 @@ const TodoListItem = ({ id, taskId, taskStatus: defaultTaskStatus, details: defa
     void database.todoListItems.where('id').equals(id).modify({ details: event.target.value })
     console.log('event.target.value', event.target.value)
     setDetails(event.target.value)
-  }
+  }, [id])
 
   return (
-    <Container css={wrapperCSS}>
+    <Card css={wrapperCSS}>
       <Box css={headerCSS}>
-        <Typography variant="h3">{taskTitle} - {projectTitle}</Typography>
-        <ToggleButtonGroup
-          value={taskStatus}
-          exclusive
-          onChange={handleStatusChange}
-          aria-label="text alignment"
-        >
-          <ToggleButton value={ETaskStatus.NEW}>
-            <Tooltip title="Pending">
-              <PendingIcon />
-            </Tooltip>
-          </ToggleButton>
-          <ToggleButton value={ETaskStatus.IN_PROGRESS}>
-            <Tooltip title="In Progress">
-              <PsychologyIcon />
-            </Tooltip>
-          </ToggleButton>
-          <ToggleButton value={ETaskStatus.BLOCKED}>
-            <Tooltip title="Blocked">
-              <PauseCircleFilledIcon />
-            </Tooltip>
-          </ToggleButton>
-          <ToggleButton value={ETaskStatus.COMPLETED}>
-            <Tooltip title="Completed">
-              <CheckCircleOutlineIcon />
-            </Tooltip>
-          </ToggleButton>
-          <ToggleButton value={ETaskStatus.CANCELED}>
-            <Tooltip title="Canceled">
-              <CancelIcon />
-            </Tooltip>
-          </ToggleButton>
-        </ToggleButtonGroup>
+        <Typography variant="h4">{taskTitle} - {projectTitle}</Typography>
+        <Box css={rightHeaderCSS}>
+          <FormControlLabel control={<Switch checked={showDetails} onChange={toggleShowDetails} />} label="Show Details" />
+          <ToggleButtonGroup
+            value={taskStatus}
+            exclusive
+            onChange={handleStatusChange}
+            aria-label="text alignment"
+          >
+            <ToggleButton value={ETaskStatus.NEW}>
+              <Tooltip title="Pending">
+                <PendingIcon />
+              </Tooltip>
+            </ToggleButton>
+            <ToggleButton value={ETaskStatus.IN_PROGRESS}>
+              <Tooltip title="In Progress">
+                <PsychologyIcon />
+              </Tooltip>
+            </ToggleButton>
+            <ToggleButton value={ETaskStatus.BLOCKED}>
+              <Tooltip title="Blocked">
+                <PauseCircleFilledIcon />
+              </Tooltip>
+            </ToggleButton>
+            <ToggleButton value={ETaskStatus.COMPLETED}>
+              <Tooltip title="Completed">
+                <CheckCircleOutlineIcon />
+              </Tooltip>
+            </ToggleButton>
+            <ToggleButton value={ETaskStatus.CANCELED}>
+              <Tooltip title="Canceled">
+                <CancelIcon />
+              </Tooltip>
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </Box>
       </Box>
-      <TextField fullWidth multiline value={details} onChange={handleDetailsChange} />
-    </Container>
+      {showDetails && <TextField fullWidth multiline value={details} onChange={handleDetailsChange} />}
+    </Card>
   )
 }
+
+const rightHeaderCSS = css`
+  width: 410px;
+  min-width: 410px;
+  display: flex;
+  align-items: center;
+`
 
 const headerCSS = css`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 1rem;
+  margin-bottom: 0.5rem;
 `
 
 const wrapperCSS = css`
-  border: 2px solid black;
-  border-radius: 5px;
-  padding: 1rem;
-  margin: 1rem 0;
+  background-color: var(--mui-palette-primary-main);
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+  margin: 0.5rem;
 `
 
 export default TodoListItem
