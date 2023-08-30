636d5db91910e4190a85afd5c458e419050505b4
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-08-29 21:03:49 -0600

    manage tasks looking much cleaner

--- frontend/src/modals/ManageTasksModal.tsx
+++ frontend/src/modals/ManageTasksModal.tsx
@@ -1,31 +1,26 @@
-import React, { useCallback, useContext } from 'react'
+import React, { useCallback, useContext, useMemo, useState } from 'react'
 import { v4 as uuid4 } from 'uuid'
 import { useLiveQuery } from 'dexie-react-hooks'
-import { Button, Typography } from '@mui/material'
+import { Box, Button, Card, IconButton, ToggleButton, Tooltip, Typography, css } from '@mui/material'
+import NotesIcon from '@mui/icons-material/Notes'
+import ShortTextIcon from '@mui/icons-material/ShortText'
+import CheckIcon from '@mui/icons-material/Check'
 
 import Modal from './Modal'
 import { EmptyStateDisplay } from 'sharedComponents'
-import { formatDateDisplayString, taskStatusLookup } from 'utilities'
-import { type TProject, type TTodoListItem, ETaskStatus, EProjectStatus, type TTask } from 'sharedTypes'
+import { type TProject, EProjectStatus, type TTask } from 'sharedTypes'
 import database from 'database'
 import { context } from 'Context'
 
-const getTaskIdsToTodoListIds = (todoListItems: TTodoListItem[]) => {
-  return todoListItems.reduce<Record<string, string>>((accumulator, { taskId, id }) => {
-    accumulator[taskId] = id
-    return accumulator
-  }, {})
+interface TaskProps {
+  task: TTask
+  isSelected: boolean
 }
 
-interface TasksByProjectTableProps {
-  project: TProject
-  tasks: TTask[]
-  taskIdsToTodoListIds: Record<string, string>
-}
-
-const TasksByProjectTable = ({ project, tasks, taskIdsToTodoListIds }: TasksByProjectTableProps) => {
+const Task = ({ task, isSelected }: TaskProps) => {
   const { state } = useContext(context)
-  const handleSelect = async ({ projectId, taskId }: { projectId: string, taskId: string }) => {
+
+  const handleSelect = async () => {
     const lastTodoListItem = (await database
       .todoListItems
       .where('todoListDate')
@@ -34,7 +29,7 @@ const TasksByProjectTable = ({ project, tasks, taskIdsToTodoListIds }: TasksByPr
       .sortBy('sortOrder'))[0]
 
     await database.todoListItems.add({
-      taskId,
+      taskId: task.id,
       id: uuid4(),
       todoListDate: state.selectedDate,
       details: '',
@@ -42,149 +37,132 @@ const TasksByProjectTable = ({ project, tasks, taskIdsToTodoListIds }: TasksByPr
     })
   }
 
-  const handleDeselect = async (todoListItemId: string) => {
-    await database.todoListItems.where({ id: todoListItemId }).delete()
+  const handleDeselect = async () => {
+    await database.todoListItems
+      .where('taskId').equals(task.id)
+      .and(item => item.todoListDate === state.selectedDate)
+      .delete()
   }
 
   return (
-    <>
-      <Typography variant="h4">{project.title}</Typography>
-      {
-        tasks.length === 0
-          ? (<EmptyStateDisplay message="No tasks for this project!" />)
-          : (
-            <table>
-              <thead>
-                <tr>
-                  <th>Task</th>
-                  <th>Status</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  tasks
-                    .sort((a, b) => {
-                      if (a.title.toLowerCase() < b.title.toLowerCase()) return -1
-                      if (a.title.toLowerCase() > b.title.toLowerCase()) return 1
-                      return 0
-                    })
-                    .map(({ title, id: taskId, status }) => {
-                      return (
-                        <tr key={taskId}>
-                          <td>{title}</td>
-                          <td>{taskStatusLookup[status]}</td>
-                          <td>
-                            {
-                              Object.keys(taskIdsToTodoListIds).includes(taskId)
-                                ? (
-                                  <Button
-                                    style={{ width: '100px' }}
-                                    key="deselect"
+    <Box css={tasksHeaderCSS}>
+      <Typography variant="body1">{task.title}</Typography>
+      <Box css={rightHeaderCSS}>
+        <IconButton color="primary" onClick={isSelected ? handleDeselect : handleSelect}>
+          <CheckIcon fontSize="small" />
+        </IconButton>
 
-                                    onClick={async () => { await handleDeselect(taskIdsToTodoListIds[taskId]) }}
-                                  >
-                                    Deselect
-                                  </Button>
-                                )
-                                : (
-                                  <Button
-                                    style={{ width: '100px' }}
-                                    key="select"
-
-                                    onClick={async () => { await handleSelect({ projectId: project.id, taskId }) }}
-                                  >
-                                    Select
-                                  </Button>
-                                )
-                            }
-                          </td>
-                        </tr>
-                      )
-                    })
-                }
-              </tbody>
-            </table>
-          )
-      }
-    </>
+      </Box>
+    </Box>
   )
 }
 
+interface ProjectProps {
+  project: TProject
+  selectedTaskIds: string[]
+}
+
+const Project = ({ project, selectedTaskIds }: ProjectProps) => {
+  const [showTasks, setShowTasks] = useState(true)
+  const toggleShowTasks = useCallback(() => { setShowTasks(prev => !prev) }, [])
+  const tasks = useLiveQuery(async () => await database.tasks.where({ projectId: project.id }).toArray())
+
+  const content = useMemo(() => {
+    if (!showTasks) {
+      return null
+    }
+
+    return (tasks ?? []).map(task => <Task isSelected={selectedTaskIds.includes(task.id)} key={task.id} task={task} />)
+  }, [tasks, showTasks, selectedTaskIds])
+
+  return (
+    <Card css={wrapperCSS}>
+      <Box css={projectHeaderCSS(showTasks)}>
+        <Typography variant="h4">{project.title}</Typography>
+        <Box css={rightHeaderCSS}>
+          <ToggleButton
+            size='small'
+            value="text"
+            onChange={toggleShowTasks}
+          >
+            <Tooltip title="Toggle Tasks">
+              {tasks && tasks.length > 0 ? <NotesIcon fontSize="small" /> : <ShortTextIcon fontSize="small" />}
+            </Tooltip>
+          </ToggleButton>
+        </Box>
+      </Box>
+      {content}
+    </Card>
+  )
+}
+
+const rightHeaderCSS = css`
+  margin-left: 1rem;
+  display: flex;
+  align-items: center;
+  flex-shrink: 0;
+`
+
+const projectHeaderCSS = (showDetails: boolean) => css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: ${showDetails ? '0.5rem' : 0};
+`
+
+const tasksHeaderCSS = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const wrapperCSS = css`
+  background-color: var(--mui-palette-primary-dark);
+  color: var(--mui-palette-secondary-main);
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+  margin: 0.5rem 0;
+`
+
 const ManageTodoListItemsModal = () => {
   const { state, dispatch } = useContext(context)
-  const tasksByProject = useLiveQuery(async () => {
-    const projects = await database
-      .projects
-      .where('status')
-      .anyOf(EProjectStatus.ACTIVE)
-      .toArray()
+  const projects = useLiveQuery(async () => await database.projects.where('status').anyOf(EProjectStatus.ACTIVE).toArray())
 
-    return await Promise.all(projects.map(async (project) => {
-      const tasks = await database.tasks
-        .where({ projectId: project.id })
-        .and((item) => [
-          ETaskStatus.NEW,
-          ETaskStatus.IN_PROGRESS,
-          ETaskStatus.BLOCKED
-        ].includes(item.status))
-        .toArray()
-
-      return {
-        project,
-        tasks
-      }
-    }))
-  }, [])
-
-  const tasksWithProject = useLiveQuery(async () => {
-    const tasks = await database.tasks.where('status').anyOf(ETaskStatus.NEW, ETaskStatus.IN_PROGRESS).toArray()
-    return await Promise.all(tasks.map(async (task) => {
-      const project = (await database.projects.where({ id: task.projectId }).first()) as TProject
-      return {
-        projectTitle: project.title,
-        ...task
-      }
-    }))
-  })
   const todoListItems = useLiveQuery(async () => await database.todoListItems.where({ todoListDate: state.selectedDate }).toArray(), [state.selectedDate])
-
-  const taskIdsToTodoListIds = getTaskIdsToTodoListIds(todoListItems ?? [])
+  const selectedTaskIds = todoListItems?.map(({ taskId }) => taskId)
 
   const handleCancel = useCallback(() => {
     dispatch({ type: 'CLEAR_ACTIVE_MODAL' })
   }, [dispatch])
 
-  let Content
+  const content = useMemo(() => {
+    if (!projects || projects.length === 0) {
+      return <EmptyStateDisplay message="No projects" />
+    }
 
-  if (!tasksWithProject?.length || !todoListItems || !tasksByProject || !tasksByProject.length) {
-    Content = <EmptyStateDisplay message="Looks like you've got nothing to do!" />
-  } else {
-    Content = (
+    return (
       <>
         {
-          tasksByProject
-            .filter(({ tasks }) => tasks.length > 0)
-            .map(({ project, tasks }) => (
-              <TasksByProjectTable
+          projects
+            .map(project => (
+              <Project
                 key={project.id}
                 project={project}
-                tasks={tasks}
-                taskIdsToTodoListIds={taskIdsToTodoListIds}
+                selectedTaskIds={selectedTaskIds ?? []}
               />
             ))
         }
         <Button fullWidth variant='contained' key="finished" onClick={handleCancel}>Done!</Button>
       </>
     )
-  }
+  }, [handleCancel, projects, selectedTaskIds])
 
   return (
     <Modal
-      title={`Select Tasks for ${formatDateDisplayString(state.selectedDate)}`}
+      title='Select Tasks'
       showModal={true}
     >
-      {Content}
+      {content}
     </Modal>
   )
 }
