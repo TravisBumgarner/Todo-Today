ca8d5b014ab5ffca9ce04fc123a4c6b2bb8afd3d
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-09-18 22:51:17 -0600

    removed not needed alias

--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -13,18 +13,7 @@ export const mainConfig: Configuration = {
   module: {
     rules,
   },
-
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
-    alias: {
-      sharedComponents: path.resolve(__dirname, 'src/react-src/sharedComponents/'),
-      modals: path.resolve(__dirname, 'src/react-src/modals/'),
-      sharedTypes: path.resolve(__dirname, 'src/react-src/sharedTypes.ts'),
-      theme: path.resolve(__dirname, 'src/react-src/theme.tsx'),
-      colors: path.resolve(__dirname, 'src/react-src/colors.tsx'),
-      utilities: path.resolve(__dirname, 'src/react-src/utilities.ts'),
-      Context: path.resolve(__dirname, 'src/react-src/Context.tsx'),
-      database: path.resolve(__dirname, 'src/react-src/database.ts')
-    }
   },
 };
