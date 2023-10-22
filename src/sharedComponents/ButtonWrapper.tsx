6a9f7e60cf96661439fe583e029d056b6177bce3
Travis Bumgarner <travis.bumgarner@gmail.com>
2023-10-21 22:15:26 -0600

    pr cleanup

--- src/sharedComponents/ButtonWrapper.tsx
+++ src/sharedComponents/ButtonWrapper.tsx
@@ -1,12 +1,9 @@
-import { Stack, css } from '@mui/material'
+import { Stack } from '@mui/material'
 
 const ButtonWrapper = ({ children, isHorizontal }: { children: React.ReactNode, isHorizontal?: boolean }) => {
   return (
-    <Stack mt={2} spacing={1} direction={isHorizontal ? 'row' : 'column'} css={wrapperCSS(isHorizontal)}>{children}</Stack>
+    <Stack mt={2} spacing={1} direction={isHorizontal ? 'row' : 'column'} css={isHorizontal}>{children}</Stack>
   )
 }
-
-const wrapperCSS = (isHorizontal?: boolean) => css`
-`
 
 export default ButtonWrapper
